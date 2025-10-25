b6bd2414d1377ffb8f918c65cb7078a2778a7543
amikkus-max <amikkus@hkhk.edu.ee>
2025-10-25 14:00:42 +0300

    12

---
+++ h9_1.py
@@ -0,0 +1,12 @@
+# Harjutus 09
+
+for i in range(1,42):
+    if   i%15 == 0:
+        print(i, "TIKTAK")
+    elif i%5 == 0:
+        print(i, "TAK")
+    elif i%3 == 0:
+        print(i, "TIK")
+    else:
+        print(i, end=" ")
+
