f9b2c1fe2bd0c66ab28c6cdb0bc722398e9e00f6
amikkus-max <amikkus@hkhk.edu.ee>
2025-10-27 18:46:11 +0200

    1

--- iseseisev_töö/is2_01.py
+++ iseseisev_töö/is2_01.py
@@ -5,16 +5,18 @@
 import turtle
 import math
 
+# skaleerimiseks muuda haara pikkust ja tipunurga suurust. kilpkonna kiirust saab ka muuta. use common sense
+
 kilpkonna_kiirus = 0
 haar = 350
-tipunurk = 55
+tipunurk = 72
+
 alus = math.sqrt(haar**2 + haar**2 - 2 * haar * haar * math.cos(math.radians(tipunurk)))
 tipu_poorde_nurk = 180 - (72 - tipunurk)
 aluse_poorde_nurk = 180 - (180 - tipunurk) / 2
 
 turtle.speed(kilpkonna_kiirus)
 turtle.showturtle()
-
 for _ in range(5):
     turtle.forward(haar)
     turtle.right(aluse_poorde_nurk)
@@ -22,9 +24,7 @@ for _ in range(5):
     turtle.right(aluse_poorde_nurk)
     turtle.forward(haar)
     turtle.left(tipu_poorde_nurk)
-
 turtle.right(36)
-
 for _ in range(5):
     turtle.forward(haar)
     turtle.right(aluse_poorde_nurk)
@@ -32,5 +32,4 @@ for _ in range(5):
     turtle.right(aluse_poorde_nurk)
     turtle.forward(haar)
     turtle.left(tipu_poorde_nurk)
-
 turtle.done()
