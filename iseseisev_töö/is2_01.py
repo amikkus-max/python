f7c1086affa49e92bed25e70258b4d65006aa6c7
amikkus-max <amikkus@hkhk.edu.ee>
2025-10-27 18:39:09 +0200

    1

---
+++ iseseisev_töö/is2_01.py
@@ -0,0 +1,32 @@
+import turtle
+import math
+
+kilpkonna_kiirus = 0
+haar = 350
+tipunurk = 55
+alus = math.sqrt(haar**2 + haar**2 - 2 * haar * haar * math.cos(math.radians(tipunurk)))
+tipu_poorde_nurk = 180 - (72 - tipunurk)
+aluse_poorde_nurk = 180 - (180 - tipunurk) / 2
+
+turtle.speed(kilpkonna_kiirus)
+turtle.showturtle()
+
+for _ in range(5):
+    turtle.forward(haar)
+    turtle.right(aluse_poorde_nurk)
+    turtle.forward(alus)
+    turtle.right(aluse_poorde_nurk)
+    turtle.forward(haar)
+    turtle.left(tipu_poorde_nurk)
+
+turtle.right(36)
+
+for _ in range(5):
+    turtle.forward(haar)
+    turtle.right(aluse_poorde_nurk)
+    turtle.forward(alus)
+    turtle.right(aluse_poorde_nurk)
+    turtle.forward(haar)
+    turtle.left(tipu_poorde_nurk)
+
+turtle.done()
